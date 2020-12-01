056146166b9395081abd9676dda0bf822d001550
Karibash <y.karibayashi@gmail.com>
2020-12-01 20:08:37 +0900

    Add the default project

---
+++ ViSER/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ViSER
+//
+//  Created by Karibash on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
